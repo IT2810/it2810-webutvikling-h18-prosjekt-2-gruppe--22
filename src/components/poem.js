df0fd24e2815c4688c1333a7c2ab74616a942f29
Hanna Evensen <hannaevensen2405@gmail.com>
2018-09-10 09:31:28 +0200

    #1 add fetch for poems

---
+++ src/components/poem.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+class Poem  extends Component {
+  constructor(props) {
+     super(props);
+     this.state = {
+       error: null,
+       isLoaded: false,
+       tekst: []
+     };
+   }
+
+   componentDidMount() {
+     fetch("/dikt/by.json")
+       .then(res => res.json())
+       .then(
+         (result) => {
+           this.setState({
+             isLoaded: true,
+             tekst: result.Tekst
+           });
+         },
+         // Note: it's important to handle errors here
+         // instead of a catch() block so that we don't swallow
+         // exceptions from actual bugs in components.
+         (error) => {
+           this.setState({
+             isLoaded: true,
+             error
+           });
+         }
+       )
+   }
+
+   render() {
+     const { error, isLoaded, tekst } = this.state;
+     if (error) {
+       return <div>Error: {error.message}</div>;
+     } else if (!isLoaded) {
+       return <div>Loading...</div>;
+     } else {
+       return (
+         <ul>
+           {tekst.map(tekst => (
+             <li key={tekst.Tittel}>
+               Tittel: {tekst.Tittel} Forfatter: {tekst.Forfatter}
+             </li>
+           ))}
+         </ul>
+       );
+     }
+   }
+
+}
+  export default Poem;
