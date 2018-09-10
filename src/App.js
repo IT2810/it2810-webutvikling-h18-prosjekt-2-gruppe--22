595b01132737a0a513adbc265ed4d67dabd3f5aa
heddah <heddaholmen@dhcp-10-22-45-37.wlan.ntnu.no>
2018-09-10 11:21:00 +0200

    #5 add basic gui

--- src/App.js
+++ src/App.js
@@ -6,13 +6,61 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+          <header className="App-header">
+              <h1 className="App-title">Utstillingstittel</h1>
+          </header>
+
+          <div className="kategori">
+              <div id="1">
+                  <p>Bilde</p>
+              </div>
+
+              <div id="2">
+                  <p>Tekst</p>
+              </div>
+
+              <div id="3">
+                  <p>Lyd</p>
+              </div>
+          </div>
+
+          <div className="faner">
+              <div id="f1">
+                  <button className="knapp aktiv">Fane 1</button>
+              </div>
+
+              <div id="f2">
+                  <button className="knapp">Fane 2</button>
+              </div>
+
+              <div id="f3">
+                  <button className="knapp">Fane 3</button>
+              </div>
+
+              <div id="f4">
+                  <button className="knapp">Fane 4</button>
+              </div>
+          </div>
+
+          <div className="main">
+              <div className="left">
+                  <div id="bilde">
+
+                  </div>
+              </div>
+
+              <div className="right">
+                  <div id="tekst">
+
+                  </div>
+
+                  <div id="lyd">
+
+                  </div>
+              </div>
+          </div>
+
       </div>
     );
   }
