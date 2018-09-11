2bf3fdc5a644a395a00483592816fcc5cf078d51
Hanna Evensen <hannaevensen2405@gmail.com>
2018-09-11 18:43:28 +0200

    #3 show svg depending on which radio button is checked

--- src/components/svg.js
+++ src/components/svg.js
@@ -13,7 +13,7 @@ class Svg extends Component {
     //filepath should be: {`/svg/${this.state.svgfile}.svg`}
     return (
       <div>
-          <SvgLoader src={`/svg/city.svg`} className="svg"/>
+          <SvgLoader src={`/svg/${this.state.svgfile}.svg`} className="svg"/>
         //By  <div>Icons made by <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 
         //By3 <div>Icons made by <a href="https://www.flaticon.com/authors/zlatko-najdenovski" title="Zlatko Najdenovski">Zlatko Najdenovski</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
