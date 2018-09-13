28206b5c575041b852799b60ba6d53f271044444
Hanna Evensen <hannaevensen2405@gmail.com>
2018-09-13 17:47:34 +0200

    #12 add footer

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../stylesheets/App.css';
+
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+          <p>Footer: add some content here</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import '../stylesheets/App.css';
 import Header from '../components/header.js';
 import Main from '../components/main.js';
+import Footer from '../components/footer.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
           <Header/>
           <Main/>
-
+          <Footer />
       </div>
     );
   }
