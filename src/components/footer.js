8d02538ce124da11b4ade6fbcd5838b023f61b65
Hanna Evensen <hannaevensen2405@gmail.com>
2018-09-14 12:30:56 +0200

    #23 add footer

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Footer extends Component {
+
+  render() {
+    //filepath should be: {`/svg/${this.state.svgfile}.svg`}
+    return (
+      <footer>
+          Todo: add footer here
+        </footer>
+    );
+  }
+}
+
+export default Footer;
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import logo from '../logo.svg';
 import '../stylesheets/App.css';
 import Header from '../components/header.js';
 import Main from '../components/main.js';
+import Footer from '../components/footer.js';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
           <Header />
           <Main />
+          <Footer />
       </div>
     );
   }
