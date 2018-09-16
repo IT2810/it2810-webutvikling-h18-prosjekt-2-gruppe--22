f6142b4c981906b92e6bfe2facbd31382936960e
heddah <heddaholmen@Heddas-MacBook-Pro.local>
2018-09-16 15:07:16 +0200

    #13 add radio buttons

--- src/components/checkboxes.js
+++ src/components/checkboxes.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 
-
 class Checkboxes extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            svg: '',
+            text: '',
+            sound: ''
+        }
+    }
+
+    onClick(){
+        console.log("hei");
+    }
+
 
   render() {
     //filepath should be: {`/svg/${this.state.svgfile}.svg`}
@@ -9,15 +22,30 @@ class Checkboxes extends Component {
       <div>
       <div className="kategori">
           <div id="1">
-              <p>Bilde</p>
+              <h3>Bilde</h3>
+              <form>
+                  <input type="radio" name="bilde" value="abstract"/> Abstrakt <br/>
+                  <input type="radio" name="bilde" value="city"/> By <br/>
+                  <input type="radio" name="bilde" value="landscape"/> Landskap <br/>
+              </form>
           </div>
 
           <div id="2">
-              <p>Tekst</p>
+              <h3>Tekst</h3>
+              <form action="">
+                  <input type="radio" name="tekst" value="abstract"/> Abstrakt <br/>
+                  <input type="radio" name="tekst" value="city"/> By <br/>
+                  <input type="radio" name="tekst" value="landscape"/> Landskap <br/>
+              </form>
           </div>
 
           <div id="3">
-              <p>Lyd</p>
+              <h3>Lyd</h3>
+              <form action="">
+                  <input type="radio" name="lyd" value="abstract"/> Abstrakt <br/>
+                  <input type="radio" name="lyd" value="city"/> By <br/>
+                  <input type="radio" name="lyd" value="landscape"/> Landskap <br/>
+              </form>
           </div>
       </div>
         </div>
