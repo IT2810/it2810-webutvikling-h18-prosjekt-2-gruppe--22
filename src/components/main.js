e387d29574dc45e027c798ccf599091957383c28
Hanna Evensen <hannaevensen2405@gmail.com>
2018-09-16 17:59:23 +0200

    #14 made the tabs work for poems

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Checkboxes from './checkboxes.js'
-import Pages from './pages.js';
+import Pages from '../containers/pages.js';
 import Svg from './svg.js';
 import Poem from '../containers/poemContainer.js';
 
@@ -12,21 +12,6 @@ class Main extends Component {
       <div>
           <Checkboxes />
           <Pages />
-          <div className="main">
-              <div className="left">
-                  <Svg />
-              </div>
-
-              <div className="right">
-                  <Poem poem="abstrakt"/>
-
-                  //TODO Add sound here
-                  <div id="lyd">
-
-                  </div>
-              </div>
-          </div>
-
         </div>
     );
   }
