c7c3eae825583ed41f6c34bf5e144f588189f2da
Hanna Evensen <hannaevensen2405@gmail.com>
2018-09-16 18:06:42 +0200

    #14 fix so that the correct poem gets shown depending on which tab is clicked

--- src/components/poem.js
+++ src/components/poem.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 
 class Poem  extends Component {
 
-
+  
    render() {
-
+     let f = 1
        return (
          <ul>
-           {this.props.tekst.map(tekst => (
-             <li key={tekst.Tittel}>
-               Tittel: {tekst.Tittel} Forfatter: {tekst.Forfatter}
-             </li>
-           ))}
+           {this.props.tekst.map((tekst,index) => {
+             if(this.props.nr == index)
+                 return <li key={tekst.Tittel}>
+                   {index} Tittel: {tekst.Tittel} Forfatter: {tekst.Forfatter}
+                 </li>
+           }
+           )}
          </ul>
        );
      }
